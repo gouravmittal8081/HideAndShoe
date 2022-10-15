c59b553a97a934945287ed0f53555ee7c4754da4
gourav mittal <107996221+gouravmittal8081@users.noreply.github.com>
2022-10-15 10:56:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import React from "react";
+
+export default function App() {
+  // yha hamne us state ko dikhaya hai and jo true hai uska mean ham us elements ko dikhana chahte hai by default
+  let [status, setStatus] = React.useState(true);
+
+  return (
+    <div className="App">
+      {
+        // agar status true hai to ye h4 tag wale ko show karega otherwise "fal " dega
+        status ? <h4>Hide And Show Program</h4> : "fal"
+      }
+      {/* yaha agar vo show ho rha hai to ye use false kr dega means hide kr dega and false hoga to true kar dega */}
+      <button onClick={() => setStatus(false)}>Hide</button>
+      <button onClick={() => setStatus(true)}>Show</button>
+    </div>
+  );
+}
