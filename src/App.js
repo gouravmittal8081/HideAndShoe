422a4b86558ddf047633579514b1e25d82ca0e1b
gourav mittal <107996221+gouravmittal8081@users.noreply.github.com>
2022-10-15 11:03:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ export default function App() {
       {/* yaha agar vo show ho rha hai to ye use false kr dega means hide kr dega and false hoga to true kar dega */}
       <button onClick={() => setStatus(false)}>Hide</button>
       <button onClick={() => setStatus(true)}>Show</button>
+      
+      {/* Agar ek hi button pr karwana hoto apko ,yha ye dekhega ki status not hai ya nhi agar hai to use ye hide kar dega or hide hoga to show kr dega*/}
+{*/<button onClick={() => setStatus(!status)}>toggle</button>*/}
     </div>
   );
 }
